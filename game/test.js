ba93bf00d4588557fecc2e828e5c2fc20daaa0b3
CJ R <hello@cjr.co.de>
2016-07-19 14:00:37 -0700

    remove failing tests!

--- game/test.js
+++ game/test.js
@@ -2,12 +2,7 @@ var suite = require('./implementation');
 var expect = require('chai').expect;
 
 describe('TDD Game', function() {
-
   it('should return true if we are ready to have fun!', function() {
     expect(suite.connected()).to.equal(true);
-  });
-
-  it('true should be true', function() {
-    expect(true).to.equal(false);
   });
 });
