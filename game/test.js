fb42e063d4de54e3177dc05b82fd031e3d78019b
CJ R <hello@cjr.co.de>
2016-07-19 13:41:08 -0700

    failing test!

--- game/test.js
+++ game/test.js
@@ -6,4 +6,8 @@ describe('TDD Game', function() {
   it('should return true if we are ready to have fun!', function() {
     expect(suite.connected()).to.equal(true);
   });
+
+  it('true should be true', function() {
+    expect(true).to.equal(false);
+  });
 });
