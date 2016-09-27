50b3de84e069f03ee80548dd917b3af2bd1e64b3
CJ R <hello@cjr.co.de>
2016-09-27 14:44:15 -0600

    :new: add failing test!

--- game/test.js
+++ game/test.js
@@ -3,6 +3,6 @@ var expect = require('chai').expect;
 
 describe('TDD Game', function() {
   it('should return true if we are ready to have fun!', function() {
-    expect(suite.connected()).to.equal(true);
+    expect(suite.connected()).to.equal(false);
   });
 });
